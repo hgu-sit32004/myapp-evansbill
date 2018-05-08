d3f1f28d6659a43061deb9a5d894e3843e26f8b7
최용규 <yg@choeyong-gyuui-MacBook-Pro.local>
2018-05-08 14:09:57 +0900

    Initial Commit

---
+++ The bucket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The bucket
+//
+//  Created by 최용규 on 2018. 4. 10..
+//  Copyright © 2018년 YG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ The bucketTests/The_bucketTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_bucketTests.swift
+//  The bucketTests
+//
+//  Created by 최용규 on 2018. 4. 10..
+//  Copyright © 2018년 YG. All rights reserved.
+//
+
+import XCTest
+@testable import The_bucket
+
+class The_bucketTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
