98473edeffe1c1a83a5704d09597e8acc95be296
최용규 <yg@choeyong-gyuui-MacBook-Pro.local>
2018-05-16 02:57:50 +0900

    before_datasource

---
+++ The bucket/First.swift
@@ -0,0 +1,35 @@
+//
+//  First.swift
+//  The bucket
+//
+//  Created by 최용규 on 2018. 5. 16..
+//  Copyright © 2018년 YG. All rights reserved.
+//
+
+import UIKit
+
+class First: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
